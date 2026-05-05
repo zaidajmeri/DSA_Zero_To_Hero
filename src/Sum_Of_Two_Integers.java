6a3efd038b9a199bb7ec287fc3bd90f5a6818c55
zaidajmeri <zaidzaidajmeri@gmail.com>
2026-05-05 23:24:42 +0530

    Initial commit: Add Sum_Of_Two_Integers Java Program

---
+++ src/Sum_Of_Two_Integers.java
@@ -0,0 +1,13 @@
+public class Sum_Of_Two_Integers {
+
+    //function for Sum of Two Number
+    public static void sum(int a, int b){
+        int add = a + b;
+        System.out.println("The Sum of Two Number is : " + add);
+    }
+
+
+    public static void main(String[] args) {
+        sum(5,5);
+    }
+}
